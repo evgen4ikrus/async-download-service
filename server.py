ccc839ef6231045a76834776c4f2865036353eda
Evgeniy Shesterikov <67272160+evgen4ikrus@users.noreply.github.com>
2023-03-09 17:05:29 +0300

    add the ability to download the archive

--- server.py
+++ server.py
@@ -1,10 +1,9 @@
 import asyncio
 import datetime
+import os.path
 
 import aiofiles
 from aiohttp import web
-
-INTERVAL_SECS = 1
 
 
 async def archive(request):
@@ -13,18 +12,22 @@ async def archive(request):
     # Большинство браузеров не отрисовывают частично загруженный контент, только если это не HTML.
     # Поэтому отправляем клиенту именно HTML, указываем это в Content-Type.
     response.headers['Content-Type'] = 'text/html'
+    response.headers['Content-Disposition'] = 'attachment; filename="photos.zip"'
 
     # Отправляет клиенту HTTP заголовки
     await response.prepare(request)
-
-    while True:
-        formatted_date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        message = f'{formatted_date}<br>'  # <br> — HTML тег переноса строки
-
-        # Отправляет клиенту очередную порцию ответа
-        await response.write(message.encode('utf-8'))
-
-        await asyncio.sleep(INTERVAL_SECS)
+    archive_hash = request.match_info.get('archive_hash')
+    archive_path = os.path.join('test_photos', archive_hash)
+    process = await asyncio.create_subprocess_exec(
+        'zip', '-r', '-', '.',
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE,
+        cwd=archive_path
+    )
+    while not process.stdout.at_eof():
+        stdout_part = await process.stdout.read(100 * 1024)
+        await response.write(stdout_part)
+    return response
 
 
 async def handle_index_page(request):
